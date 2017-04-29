f773cc619f95706469929700c5fdfa1ee54f372a
draden05 <rodolphe.lebrun@ynov.com>
2017-04-29 22:57:21 +0200

    22:57

--- src/ENT/SiteBundle/Controller/LoginController.php
+++ src/ENT/SiteBundle/Controller/LoginController.php
@@ -43,8 +43,36 @@ class LoginController extends Controller
 
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             //  var_dump();
-            $content = "bouh";
-            return new Response($content);//$this->redirect($this->generateUrl('panel_membre'));
+            $repository = $this->getDoctrine()
+              ->getManager()
+              ->getRepository('ENTSiteBundle:Contact');
+              //var_dump($repository);
+            // On récupère l'entité correspondante à l'id $id
+            $find = $repository->findAll($pseudo);
+            //var_dump($documentation);
+            $error = "TEST DONE !";
+            // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
+            // ou null si l'id $id  n'existe pas, d'où ce if :
+            if (null === $find) {
+                $error = "L'annonce d'id ".$pseudo." n'existe pas.";
+            } else {
+                $pseu = $find->getPseudo();
+          //      $id_doc = $documentation->getId();
+          //      $id_cat = $documentation->getIdCat();
+          //      $nom = $documentation->getNom();
+          //      $taille = $documentation->getTaille();
+           // $date_enrg = $documentation->getDataEnregistrement();
+
+            // Le render ne change pas, on passait avant un tableau, maintenant un objet
+            return $this->render('ENTSiteBundle:Membre:panelmembre.html.twig', array(
+              'pseudo' => $pseu,
+              //'data_enrg' => $data_enrg,
+              'test' => $error,
+            ));
+            }
+            return $this->render('ENTSiteBundle:Membre:login.html.twig', array(
+                 'test' => $error,
+               ));
         }
         return $this->render('ENTSiteBundle:Membre:login.html.twig', array('form' => $form->createView()));
     //    $content = $this
