1a148c768ee266b00e5574b0591d2a04f1272922
draden05 <rodolphe.lebrun@ynov.com>
2017-05-01 20:01:27 +0200

    freg + sql update

--- bdd.sql
+++ bdd.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
 -- version 4.6.4
 -- https://www.phpmyadmin.net/
--- author : SpaceTeam
+--
 -- Client :  127.0.0.1
--- Généré le :  Jeu 26 Avril 2017 à 22:34
+-- Généré le :  Lun 01 Mai 2017 à 18:00
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `bdd`
 --
-CREATE DATABASE IF NOT EXISTS `bdd` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-  USE `bdd`;
 
 -- --------------------------------------------------------
 
@@ -40,8 +38,14 @@ CREATE TABLE `documentation` (
 -- Contenu de la table `documentation`
 --
 
-INSERT INTO `documentation` (`id_documentation`, `id_cat`, `nom`,`taille`, `date_enregistrement`) VALUES
-(1, 2, 'Exemple_doc.pdf', 642, '1992-07-10 14:44:46');
+INSERT INTO `documentation` (`id_documentation`, `id_cat`, `nom`, `taille`, `date_enregistrement`) VALUES
+(1, 2, 'Exemple_doc.pdf', 642, '1992-07-10'),
+(2, 2, 'Exemple_doc02.pdf', 355, '1992-07-10'),
+(3, 2, 'Exemple_doc03.pdf', 585, '1992-07-10'),
+(4, 1, 'Exemple_doc2525.pdf', 157, '1992-07-10'),
+(5, 1, 'Exemple_doc2555525.pdf', 963, '1992-07-10'),
+(6, 3, 'Exemple_doc445525.pdf', 453, '1992-07-10'),
+(7, 4, 'Exemple_doc455525.pdf', 256, '1992-07-10');
 
 -- --------------------------------------------------------
 
@@ -62,7 +66,28 @@ CREATE TABLE `logiciels` (
 --
 
 INSERT INTO `logiciels` (`id_logiciels`, `id_cat`, `nom`, `taille`, `date_enregistrement`) VALUES
-(1, 2, 'exemple_log.exe', 1145, '1992-07-10 18:48:42');
+(1, 2, 'exemple_log.exe', 1145, '1992-07-10');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiel`
+--
+
+CREATE TABLE `materiel` (
+  `id_materiel` int(3) NOT NULL,
+  `id_membre` int(3) NOT NULL,
+  `id_cat` int(1) NOT NULL,
+  `nom` varchar(40) NOT NULL,
+  `number` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `materiel`
+--
+
+INSERT INTO `materiel` (`id_materiel`, `id_membre`, `id_cat`, `nom`, `number`) VALUES
+(3, 1, 2, '1060 6Go', 'M0148');
 
 -- --------------------------------------------------------
 
@@ -71,22 +96,6 @@ INSERT INTO `logiciels` (`id_logiciels`, `id_cat`, `nom`, `taille`, `date_enregi
 --
 
 CREATE TABLE `membre` (
-  'id'
-'password'
-'roles'
-'username' 
-'usernameCanonical'
-'email'
-'emailCanonical'
-'enabled'
-'salt'
-'plainPassword'
-'lastLogin'
-'confirmationToken'
-'passwordRequestedAt'
-'groups'
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
   `id_membre` int(3) NOT NULL,
   `pseudo` varchar(20) NOT NULL,
   `mdp` varchar(32) NOT NULL,
@@ -98,33 +107,50 @@ PRIMARY KEY (`id`)
   `ville` varchar(20) NOT NULL,
   `code_postal` int(5) UNSIGNED ZEROFILL NOT NULL,
   `adresse` varchar(50) NOT NULL,
-  `statut` int(1) NOT NULL DEFAULT '0'
-
+  `statut` int(1) NOT NULL DEFAULT '0',
+  `is_active` varchar(100) DEFAULT 'ROLE_USER',
+  `salt` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `membre`
 --
 
-INSERT INTO `membre` (`id_membre`, `pseudo`, `mdp`, `nom`, `prenom`, `email`, `tel`, `civilite`, `ville`, `code_postal`, `adresse`, `statut`) VALUES
-(1, 'juju', 'soleil', 'Cottet', 'Julien', 'julien.cottet@gmail.com', 061122339900, 'm', 'Marseille', 13010, '300 rue de la chance', 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `materiel`
---
-
-CREATE TABLE `materiel`(
-`id_materiel` int(3) NOT NULL,
-`id_membre` int(3) NOT NULL,
-`id_cat` int(1) NOT NULL,
-`nom` varchar(40) NOT NULL,
-`number` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `membre` (`id_membre`, `pseudo`, `mdp`, `nom`, `prenom`, `email`, `tel`, `civilite`, `ville`, `code_postal`, `adresse`, `statut`, `is_active`, `salt`) VALUES
+(1, 'juju', 'soleil', 'Cottet', 'Julien', 'julien.cottet@gmail.com', 2147483647, 'm', 'Marseille', 13010, '300 rue de la chance', 0, 'ROLE_USER', 'v1-s@lt'),
+(2, 'admin', 'admin', 'ad', 'min', 'admin@gmail.com', 610672054, 'm', 'Aix', 13100, '2 rue le corbusier', 1, 'ROLE_ADMIN', 'v1-s@lt');
 
 --
--- Contenu de la table `materiel`
+-- Index pour les tables exportées
 --
 
-INSERT INTO `materiel` (`id_materiel`, `id_membre`, `id_cat`, `nom`, `number`) VALUES
-(3, 1, 2, '1060 6Go', 'M0148');
+--
+-- Index pour la table `documentation`
+--
+ALTER TABLE `documentation`
+  ADD PRIMARY KEY (`id_documentation`);
+
+--
+-- Index pour la table `materiel`
+--
+ALTER TABLE `materiel`
+  ADD PRIMARY KEY (`id_materiel`);
+
+--
+-- Index pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD PRIMARY KEY (`id_membre`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `membre`
+--
+ALTER TABLE `membre`
+  MODIFY `id_membre` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
